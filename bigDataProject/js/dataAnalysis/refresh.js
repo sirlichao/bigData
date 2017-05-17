ce9596a89360203c3e3d77e7cf618e9cf89e2051
sirlichao <346130036@qq.com>
2017-05-17 15:55:07 +0800

    完善了数据刷新页面

--- bigDataProject/js/dataAnalysis/refresh.js
+++ bigDataProject/js/dataAnalysis/refresh.js
@@ -2,11 +2,13 @@
  * Created by lc on 2017/5/16.
  */
 $(function(){
+    //var target;
     $('.refresh').on('click',function(e){
         if($("#popover").is(":hidden")){
             //如果隐藏组织冒泡
             e.stopPropagation();
         };
+        $(this).addClass('addRefresh')
         //获取元素相对于body的位置
         var x = $(this).offset().top;
         var y = $(this).offset().left;
@@ -16,6 +18,7 @@ $(function(){
             'top':(x-92)+'px',
             'left':(y+27)+'px'
         });
+        //target = e.target;
     })
     $('#remove').on('click',function(){
         $('#popover').hide();
@@ -39,9 +42,14 @@ $(function(){
     $('#popover').on('click',function(e){
         e.stopPropagation();
     });
-    $('#btnRefresh').on('click',function(){
+    $('#btnRefresh').on('click',function(e){
         $.ajax({});//发送请求
         //获取选中input的value值
+        //$(target).addClass('animation');
+        //请求数据是添加class
+        $('.addRefresh').addClass('animation').removeClass('addRefresh')
+        //请求完成时remove掉animation这个class
+        //$('.refresh').removeClass('animation')
         var refVal = $('input:checked').val();
         $('input[name="refresh"]').each(function(index,elem){
             elem.checked =false;
